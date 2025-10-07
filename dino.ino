3a5bf29beaafa2e65ff8dfec34b08ba857772de7
KeyartDev <160310908+KeyartDev@users.noreply.github.com>
2025-10-07 10:15:10 +0300

    Update dino.ino

--- dino.ino
+++ dino.ino
@@ -1,5 +1,6 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <EEPROM.h>
 #define printByte(args)  write(args);
 #define JUMPD 300
 #define EMPTY 0
@@ -7,7 +8,7 @@
 #define SUSROAD 2
 #define CACROAD 3
 #define SUSJUMP 4
-#define FUNC pow(millis()/50,1/3)
+#define FUNC pow(millis()/100,1/3)
 #define ARR 8
 
 
@@ -20,11 +21,13 @@ uint8_t cac_road[8] = {0x4,0x15,0x15,0x15,0x1f,0x4,0x4,0x1f};
 uint8_t empty[8] = {};
 uint8_t road_misc[8] = {};
 
+void(* reset)(void) = 0;
 
 volatile static uint32_t tmr;
 volatile static uint32_t tmr1;
 volatile static uint32_t tmr2;
 volatile bool isJumped = false;
+volatile bool isEnd = false;
 volatile int arrSize = 0;
 volatile int creationDelay = 0;
 volatile bool isGameOver = false;
@@ -34,6 +37,9 @@ volatile int cacCount = 0;
 volatile int lastCreationDelay = 0;
 
 volatile int susPos = 2;
+volatile int score = 0;
+
+volatile int scores[3] = {0, 0, 0};
 
 class Cactus;
 
@@ -74,7 +80,11 @@ public:
     if (this->column <= 0) {
       this->setEnded(true);
       cacCount--;
-      
+      score+=3;
+      lcd.setCursor(16,0);
+      lcd.print("0000");
+      lcd.setCursor(20-String(score).length(),0);
+      lcd.print(String(score));
     }
     
     lcd.setCursor(getColumn(), getRow());
@@ -109,26 +119,48 @@ void removeCac(Cactus* cac) {
   cacCount--;
   lcd.setCursor(0,2);
   lcd.printByte(ROAD);
+  
+  
 }
 
 void onClick() {
   Serial.println("Jump!");
   if (!isJumped) isJumped = true;
+  if (isGameOver) {
+    Serial.write('3');
+    reset();
+  }
+}
+void onReset() {
+  if (!isGameOver) return;
+  
+  Serial.println("PRERIVANIE!!!");
+  for (int i=0;i<3;i++) {
+    scores[i]=0;
+    EEPROM.write(i,0);
+  }
+  
+  lcd.setCursor(5,1);
+  lcd.print(scores[0] == 0 ? "1-st: --" : "1-st: " + String(scores[0]));
+  lcd.setCursor(5,2);
+  lcd.print(scores[1] == 0 ? "2-nd: --" : "2-nd: " + String(scores[1]));
+  lcd.setCursor(5,3);
+  lcd.print(scores[2] == 0 ? "3-rd: --" : "3-rd: " + String(scores[2]));
 }
 
 void setup() {
   Serial.begin(115200);
 
   pinMode(2,INPUT_PULLUP);
-  pinMode(3 , OUTPUT);
+  pinMode(3,INPUT_PULLUP);
   pinMode(4 , OUTPUT);
   pinMode(5 , OUTPUT);
-  pinMode(10,OUTPUT);
+  pinMode(6 , OUTPUT);
   pinMode(9,OUTPUT);
 
-  digitalWrite(3,HIGH);
   digitalWrite(4,HIGH);
   digitalWrite(5,HIGH);
+  digitalWrite(6,HIGH);
   
   
   lcd.init();
@@ -149,11 +181,24 @@ void setup() {
     lcd.printByte(ROAD);
   }
 
-  attachInterrupt(digitalPinToInterrupt(2), onClick, RISING);
-  digitalWrite(10,HIGH);
-  delay(100);
-  digitalWrite(10,LOW);
+  attachInterrupt(digitalPinToInterrupt(2), onClick, FALLING);
+  attachInterrupt(digitalPinToInterrupt(3), onReset, FALLING);
+  Serial.write('1');
+  lcd.setCursor(16,0);
+  lcd.print("0000");
+  isEnd = false;
+
+  for (int i=0;i<3;i++) {
+    if (EEPROM.read(i) != 255) continue;
+    EEPROM.write(i,0);
+  }
+
+  for (int i=0;i<3;i++) {
+    scores[i]=EEPROM.read(i);
+  }
 }
+
+
 
 void loop() {
   if (isJumped) {
@@ -197,7 +242,7 @@ void loop() {
     isJumped = false;
   }
   
-  if (arrSize > 0 && millis()-tmr >= 350-FUNC) {
+  if (arrSize > 0 && millis()-tmr >= 350-FUNC && !isGameOver) {
     for (int i=0;i<arrSize;i++) {
       arr[i]->moveLeft();
       lcd.setCursor(0,2);
@@ -206,7 +251,7 @@ void loop() {
     tmr = millis();
   }
 
-  if (millis()-tmr1 >= creationDelay && cacCount < ARR) {
+  if (millis()-tmr1 >= creationDelay && cacCount < ARR && !isGameOver) {
     Cactus* cac = new Cactus(19, 2);
     addCac(cac);
     randomSeed(millis());
@@ -218,7 +263,8 @@ void loop() {
 
   for (int i=0;i<arrSize;i++) {
     if (susPos == 2 && arr[i]->getColumn() == 3 && millis()-tmr2 >= 350-FUNC) {
-      digitalWrite(hearts+2,LOW);
+      digitalWrite(hearts+3,LOW);
+      if (hearts > 1) Serial.write('2');
       hearts--;
       tmr2 = millis();
       if (hearts == 0) {
@@ -228,30 +274,49 @@ void loop() {
     }
   }
 
-  if (isGameOver) {
-    digitalWrite(9,HIGH);
-    delay(10);
-    digitalWrite(9,LOW);
+  if (isGameOver && !isEnd) {
+    Serial.write('0');
     lcd.print("GAME OVER!");
     for (int r=0;r<4;r++) {
-      for (int c=0;c<20;c++) {
-        lcd.setCursor(c,r);
+      for (int h=0;h<20;h++) {
+        lcd.setCursor(h,r);
         lcd.printByte(EMPTY);
       }
     }
-    lcd.setCursor(5,1);
+    lcd.setCursor(5,0);
     lcd.print("GAME OVER!");
-    while (true) {}
+    if (score > scores[0]) {
+      scores[2]=scores[1];
+      scores[1]=scores[0];
+      scores[0]=score;
+    } else if (score > scores[1]) {
+      scores[2]=scores[1];
+      scores[1]=score;
+    } else if (score > scores[2]) {
+      scores[2]=score;
+    }
+      
+    for (int i=0;i<3;i++){
+        EEPROM.write(i,scores[i]);
+    }
+    
+    lcd.setCursor(5,1);
+    lcd.print(scores[0] == 0 ? "1-st: --" : "1-st: " + String(scores[0]));
+    lcd.setCursor(5,2);
+    lcd.print(scores[1] == 0 ? "2-nd: --" : "2-nd: " + String(scores[1]));
+    lcd.setCursor(5,3);
+    lcd.print(scores[2] == 0 ? "3-rd: --" : "3-rd: " + String(scores[2]));
+    isEnd = true;
   }
 
-  if (susPos == 2) {
+  if (susPos == 2 && !isGameOver) {
     lcd.setCursor(3,2);
     lcd.printByte(SUSROAD);
   }
 }
 
 void yield() {
-  if (arrSize > 0 && millis()-tmr >= 350-FUNC) {
+  if (arrSize > 0 && millis()-tmr >= 350-FUNC && !isGameOver) {
     for (int i=0;i<arrSize;i++) {
       arr[i]->moveLeft();
       lcd.setCursor(0,2);
@@ -260,7 +325,7 @@ void yield() {
     tmr = millis();
   }
 
-  if (millis()-tmr1 >= creationDelay && cacCount < ARR) {
+  if (millis()-tmr1 >= creationDelay && cacCount < ARR && !isGameOver) {
     Cactus* cac = new Cactus(19, 2);
     addCac(cac);
     randomSeed(millis());
@@ -272,7 +337,8 @@ void yield() {
  
   for (int i=0;i<arrSize;i++) {
     if (susPos == 2 && arr[i]->getColumn() == 3 && millis()-tmr2 >= 350-FUNC) {
-      digitalWrite(hearts+2,LOW);
+      digitalWrite(hearts+3,LOW);
+      if (hearts > 1) Serial.write('2');
       hearts--;
       tmr2 = millis();
       if (hearts == 0) {
